e35b7eafed5189adf5323f90fde18a2f84fef204
Lorenzo Colletta <id.boh06@gmail.com>
2021-11-16 15:18:26 +0100

    Implement from scrtch new methods

--- src/it/unibo/oop/lab/enum2/Sport2SocialNetworkUserImpl.java
+++ src/it/unibo/oop/lab/enum2/Sport2SocialNetworkUserImpl.java
@@ -1,7 +1,11 @@
-/**
+ /**
  * 
  */
 package it.unibo.oop.lab.enum2;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 import it.unibo.oop.lab.socialnetwork.SocialNetworkUserImpl;
 import it.unibo.oop.lab.socialnetwork.User;
@@ -30,7 +34,8 @@ public class Sport2SocialNetworkUserImpl<U extends User> extends SocialNetworkUs
      * 
      * add a field to keep track of the set of sports followed/done by a user
      */
-
+	private Set<Sport> sports;
+	
     /**
      * Builds a new {@link Sport2SocialNetworkUserImpl}.
      * 
@@ -61,6 +66,7 @@ public class Sport2SocialNetworkUserImpl<U extends User> extends SocialNetworkUs
      */
     public Sport2SocialNetworkUserImpl(final String name, final String surname, final String user, final int userAge) {
         super(name, surname, user, userAge);
+        sports = new HashSet<>();
     }
 
     /*
@@ -75,7 +81,9 @@ public class Sport2SocialNetworkUserImpl<U extends User> extends SocialNetworkUs
      *            a sport followed/done by the user
      */
     public void addSport(final Sport sport) {
-
+    	if(sport != null) {
+        	sports.add(sport);    		
+    	}
     }
 
     /**
@@ -87,7 +95,10 @@ public class Sport2SocialNetworkUserImpl<U extends User> extends SocialNetworkUs
      * @return true if user likes sport s
      */
     public boolean likesSport(final Sport s) {
-        return false;
+    	if(sports.contains(s)) {
+    		return true;
+    	}
+    	return false;
     }
 
     /*
@@ -100,18 +111,38 @@ public class Sport2SocialNetworkUserImpl<U extends User> extends SocialNetworkUs
      * 
      * @return the set of individual sport this user practices/follows
      */
-    /*
-     * public Set<Sport> getIndividualSports() { return null; }
-     * 
-     * 
-     * /** Returns the set of sports which are practiced in a given place.
+    public Set<Sport> getIndividualSports() {
+    	Set<Sport> individualSports = Set.copyOf(this.sports);
+    	Iterator<Sport> itRemove = individualSports.iterator();
+    	
+    	while(itRemove.hasNext()) {
+    		if(!itRemove.next().isIndividualSport()) {
+    			itRemove.remove();
+    		}
+    	}
+    	return individualSports;
+    }
+    
+    /** Returns the set of sports which are practiced in a given place.
      * 
      * @param p the place in which the sport is practiced in order to be
      * included in the resulting set
      * 
      * @return the set of sport practiced in a given place
      */
-    /*
-     * public Set<Sport> getSportPracticedInPlace(Place p) { return null; }
-     */
+    public Set<Sport> getSportPracticedInPlace(Place p) {
+    	Set<Sport> individualSports = Set.copyOf(this.sports);
+    	Iterator<Sport> itRemove = individualSports.iterator();
+    	
+    	while(itRemove.hasNext()) {
+    		Sport next = itRemove.next();
+    		if(next.isIndoorSport() && p == Place.OUTDOOR) {
+    			itRemove.remove();
+    		}
+    		if(!next.isIndoorSport() && p == Place.INDOOR) {
+    			itRemove.remove();
+    		}
+    	}
+    	return individualSports;	
+    }
 }
